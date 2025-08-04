97c27f3a05276b4f675d628724f25a6eb20cdc5a
M4TH3US17 <matheusdalvino50@gmail.com>
2025-08-04 15:45:50 -0400

    listagem objeto

--- public/assets/data/utils/data-utils.tsx
+++ public/assets/data/utils/data-utils.tsx
@@ -14,6 +14,8 @@ export function returnData(currentLanguage: string): object {
 
     processDescriptions(data)
 
+    console.log(data)
+
     return data
 }
 
@@ -30,9 +32,10 @@ function processDescriptions(data: any): any {
             if (key === 'description' && typeof data[key] === 'object') {
                 const description = data[key];
 
-                for (const textKey in description)
+                for (const textKey in description) {
                     if (description[textKey]?.hasOwnProperty('links'))
                         description[textKey] = adjustLink(description[textKey]);
+                }
 
             } else {
                 // Se não for description, processa recursivamente
@@ -44,10 +47,27 @@ function processDescriptions(data: any): any {
     return data;
 }
 
-function adjustLink(descriptionObject: any): object {
-    descriptionObject.links.map((link: any) => {
-        descriptionObject.text = descriptionObject.text.replace(link.breakpoint, `<a href='${link.href}'>${link.text}</a>`)
-    })
+function adjustLink(descriptionObject: any): any {
+    descriptionObject.links.map((link: any) => descriptionObject.text = descriptionObject.text.replace(link.breakpoint, `<a href='${link.href}'>${link.text}</a>`))
+    
+    const isListagem = descriptionObject.items
+    if (isListagem) {
+        const listagem = descriptionObject.items
+
+        const objeto_listagem_li_formatado = listagem.map((listItemObject: any) => {
+            let hasLinks = listItemObject.links
+
+            if (hasLinks) {
+                let links = listItemObject.links
+                links.map((link: any) => listItemObject.li = listItemObject.li.replace(link.breakpoint, `<a href='${link.href}'>${link.text}</a>`))
+                return listItemObject.li
+            }
+        })
+
+        descriptionObject.items = objeto_listagem_li_formatado
+        const { links, ...listagem_formatada } = descriptionObject;
+        return listagem_formatada;
+    }
 
     return { text: descriptionObject.text };
 }
